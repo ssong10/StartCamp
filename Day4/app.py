4ac39a1ac42339a5190d60f75219fa88855da531
2seung10 <leesy1403@naver.com>
2019-07-11 17:53:22 +0900

    190711 | Startcamp - Day 4 | Flask, Python

--- Day4/app.py
+++ Day4/app.py
@@ -1,5 +1,6 @@
 # 0 . flask 패키지 가져오기
-from flask import Flask, render_template
+import random
+from flask import Flask, render_template, request
 
 # 1. app 설정
 app = Flask(__name__)
@@ -12,6 +13,42 @@ def index():
 @app.route('/hello/<string:name>')
 def hello(name):
     return render_template('hello.html',name = name)
+
+@app.route('/lunch')
+def lunch():
+    menus = ['레드코코넛누들','소불고기' , '삼계탕', '싸이버거', '치킨']
+    pick = random.choice(menus)
+    return render_template('lunch.html',menus= menus,pick=pick)
+
+@app.route('/naver')
+def naver():
+    return render_template('naver.html')
+
+@app.route('/ping')
+def ping():
+    return render_template('ping.html')
+
+@app.route('/pong')
+def pong():
+    # 사용자가 보낸 데이터를 받아와서
+    text = request.args.get('say')
+    textext = request.args.get('saysay')
+
+    print(request.args)
+    # 템플릿에 넘겨준다.
+    return render_template('pong.html', text=text,textext=textext)
+
+@app.route('/question')
+def question():
+    return render_template('question.html')
+
+@app.route('/answer')
+def answer():
+    character = ['고독한','배고픈' , '성공한', '엉뚱한', '수십억을 가지고 있는','이상한','천재','부유한','퇴근 일찍 하는']
+    pick = random.choice(character)
+    name = request.args.get('name')
+    job = request.args.get('job')
+    return render_template('answer.html', pick=pick, job=job, name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ Day4/lotto.py
@@ -0,0 +1,62 @@
+import random
+import pprint
+import requests
+url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=866"
+
+# 1. 요청
+    # json -> 
+    # 파이썬의 dictionary와 list로 변환하여 조작할 수 있다.
+    # 응답 (HTML, XML, JSON) 3가지 배움
+response = requests.get(url).json()
+# pprint.pprint(response)
+# print(type(response))
+
+first = 0
+second = 0
+third = 0 
+fourth = 0
+fifth = 0
+bomb = 0
+
+
+lotto = []
+for i in range(1,7):
+    lotto.append(response[f'drwtNo{i}'])
+#print(lotto)
+
+a = int(input('몇 번이나 뽑을까요? : '))
+for i in range(a):
+    my_lotto = sorted(random.sample(range(1,46),6))
+    #print(my_lotto)
+
+    n=0
+    for i in my_lotto:
+        if i in lotto:
+            n += 1
+
+
+    if n == 6:
+        #print("1등입니다~~")
+        first += 1
+    elif n == 5:
+        if response['bnusNo'] in my_lotto:
+            #print("2등입니다~~")
+            second +=1 
+        else:
+            #print("3등입니다~~")
+            third += 1
+    elif n == 4:
+        #print("4등입니다~~ 50000원 받아가세요")
+        fourth += 1
+    elif n == 3:
+        #print("5등입니다~~  5000원 받아가세요")
+        fifth +=1
+    else:
+        #print("소중한 천원을 날리셨습니다~")
+        bomb +=1
+
+print(f'1등:{first}, 2등:{second}, 3등: {third}, 4등: {fourth}, 5등: {fifth}, 꽝:{bomb}')
+if first > 0 :
+    print("와~~ 1등이다")
+
+print(f'{2240409000*first + 49420787* second + 1651408* third + 50000 * fourth + 5000*fifth - 1000* a} 원 이득!')
